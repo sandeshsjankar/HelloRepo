d51f65d1c8de8bbb11277643c8562aa782c66f07
sandeshsjankar <sandeshsjankar@gmail.com>
2024-07-11 17:43:27 +0530

    first commit

---
+++ src/main/java/com/practice/hello/controller/HelloController.java
@@ -0,0 +1,13 @@
+package com.practice.hello.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello")
+    public String Hello(){
+        return "Hello Aayush Shubham";
+    }
+}
